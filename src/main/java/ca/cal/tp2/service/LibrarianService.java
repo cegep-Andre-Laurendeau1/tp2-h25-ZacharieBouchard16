524d250d982c64fe6215475ffcf530519394419f
Zacharie <Zacharie.Bouchard16@gmail.com>
2025-03-06 00:23:42 -0500

    Remplacement du type du Repository.

--- src/main/java/ca/cal/tp2/service/LibrarianService.java
+++ src/main/java/ca/cal/tp2/service/LibrarianService.java
@@ -1,14 +1,14 @@
 package ca.cal.tp2.service;
 
 import ca.cal.tp2.model.Book;
-import ca.cal.tp2.repository.BookRepository;
+import ca.cal.tp2.repository.Repository;
 import lombok.AllArgsConstructor;
 
 import java.time.LocalDate;
 
 @AllArgsConstructor
 public class LibrarianService {
-    BookRepository bookRepository;
+    Repository bookRepository;
 
     public void addBook(long id, String name, LocalDate releaseDate, int amountTotal, String isbn, int borrowTime, int pages) {
         // bookRepository.save(new Book(id, name, releaseDate, amountTotal, isbn, borrowTime, pages));
