81e630606fe07869cb1709b9ab15affb916cbeff
Zacharie <Zacharie.Bouchard16@gmail.com>
2025-03-08 23:44:23 -0500

    Correction du type après réflexion.

--- src/main/java/ca/cal/tp2/service/LibrarianService.java
+++ src/main/java/ca/cal/tp2/service/LibrarianService.java
@@ -8,13 +8,13 @@ import java.time.LocalDate;
 
 @AllArgsConstructor
 public class LibrarianService {
-    Repository bookRepository;
+    private Repository<Book> bookRepository;
 
     public void addBook(long id, String name, LocalDate releaseDate, int amountTotal, String isbn, int borrowTime, int pages) {
         // bookRepository.save(new Book(id, name, releaseDate, amountTotal, isbn, borrowTime, pages));
     }
 
     public Book getBook(long id) {
-        return (Book) bookRepository.get(id);
+        return bookRepository.get(id);
     }
 }
