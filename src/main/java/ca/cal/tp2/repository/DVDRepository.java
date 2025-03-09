dc1a36e9150ddd5f1239084084e1a1f33d727def
Zacharie <Zacharie.Bouchard16@gmail.com>
2025-03-09 00:41:27 -0500

    Création du DVD repository.

---
+++ src/main/java/ca/cal/tp2/repository/DVDRepository.java
@@ -0,0 +1,9 @@
+package ca.cal.tp2.repository;
+
+import ca.cal.tp2.model.DVD;
+
+public class DVDRepository extends RepositoryBase<DVD> {
+    public DVDRepository() {
+        super(DVD.class);
+    }
+}
