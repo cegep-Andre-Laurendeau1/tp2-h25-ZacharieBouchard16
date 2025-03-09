c430ce8e591814770ff49388638a11f8bfd74e8f
Zacharie <Zacharie.Bouchard16@gmail.com>
2025-03-09 00:37:17 -0500

    Création du CD repository.

---
+++ src/main/java/ca/cal/tp2/repository/CDRepository.java
@@ -0,0 +1,10 @@
+package ca.cal.tp2.repository;
+
+import ca.cal.tp2.model.CD;
+
+
+public class CDRepository extends RepositoryBase<CD> {
+    public CDRepository(Class<CD> entityClass) {
+        super(entityClass);
+    }
+}
