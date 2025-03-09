4657bc4ce5cad83ef7a0eda74711903cbcda24dc
Zacharie <Zacharie.Bouchard16@gmail.com>
2025-03-09 00:31:09 -0500

    Mapping en JPA de DVD.

--- src/main/java/ca/cal/tp2/model/DVD.java
+++ src/main/java/ca/cal/tp2/model/DVD.java
@@ -1,11 +1,25 @@
 package ca.cal.tp2.model;
 
-import lombok.Data;
+import jakarta.persistence.Entity;
+import lombok.*;
 
-@Data
+import java.time.LocalDate;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor(force = true)
 public class DVD extends Document{
     private String director;
     private final int length;
     private final int borrowTime;
     private final String rating;
+
+    public DVD(Long id, String name, LocalDate releaseDate, int amountTotal, String director, int length, int borrowTime, String rating) {
+        super(id, name, releaseDate, amountTotal);
+        this.director = director;
+        this.length = length;
+        this.borrowTime = borrowTime;
+        this.rating = rating;
+    }
 }
