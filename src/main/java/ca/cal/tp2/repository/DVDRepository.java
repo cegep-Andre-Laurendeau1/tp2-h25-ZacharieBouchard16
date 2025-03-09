39ba855ce29dab487744225809047072f3cfdbab
Zacharie <Zacharie.Bouchard16@gmail.com>
2025-03-09 17:52:46 -0400

    Ajout du getByYear au DVD.

--- src/main/java/ca/cal/tp2/repository/DVDRepository.java
+++ src/main/java/ca/cal/tp2/repository/DVDRepository.java
@@ -1,7 +1,11 @@
 package ca.cal.tp2.repository;
 
+import ca.cal.tp2.model.CD;
 import ca.cal.tp2.model.DVD;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.Query;
 
+import java.time.LocalDate;
 import java.util.List;
 
 public class DVDRepository extends DocumentRepository<DVD> {
@@ -11,6 +15,12 @@ public class DVDRepository extends DocumentRepository<DVD> {
 
     @Override
     public List<DVD> getByYear(int year) {
-        return null;
+        EntityManager em = emf.createEntityManager();
+
+        Query query = em.createQuery("SELECT dvd FROM DVD dvd WHERE dvd.releaseDate BETWEEN :yearStart AND :yearEnd");
+        query.setParameter("yearStart", LocalDate.of(year, 1, 1));
+        query.setParameter("yearEnd", LocalDate.of(year, 12, 31));
+        em.close();
+        return query.getResultList();
     }
 }
